528cdc211e4bcd75939545c4c3ef041061b42aec
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-09-12 01:45:27 +0900

    Update ccnrun.sh

--- ccnrun.sh
+++ ccnrun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java   -cp ./classes:ccn/:lib/commons-math-2.0.jar:lib/log4j-api-2.11.1.jar:lib/log4j-core-2.11.1.jar:lib/ncl-taskschedsim.jar:lib/ncl-sfc.jar:lib/ncl-taskschedsim.jar net.gripps.ccn.main.CCNTest ccn.properties
+java   -cp ./classes:ccn/:lib/commons-math-2.0.jar:lib/ncl-sfc.jar:lib/log4j-api-2.11.1.jar:lib/log4j-core-2.11.1.jar:lib/ncl-taskschedsim.jar:lib/ncl-sfc.jar:lib/ncl-taskschedsim.jar net.gripps.ccn.main.CCNTest ccn.properties
